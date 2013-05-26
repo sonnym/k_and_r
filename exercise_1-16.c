3bbb15f41041f4e2d4c8e59d0ef24613b3220f6f
sonnym <michaud.sonny@gmail.com>
2013-05-26 05:40:14 -0400

    exercise 1-16

--- exercise_1-16.c
+++ exercise_1-16.c
@@ -28,8 +28,10 @@ main() {
 int get_line(char s[], int lim) {
   int c, i;
 
-  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
-    s[i] = c;
+  for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i) {
+    if (i < lim - 1) {
+      s[i] = c;
+    }
   }
 
   if (c  == '\n') {
